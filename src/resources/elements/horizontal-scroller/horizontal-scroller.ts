71cbf024b3370f9953436b4dd1919ae35d627a79
Doug Kent <github@douglaskent.com>
2021-04-01 11:34:49 -0600

    animated horiz scrolling

--- src/resources/elements/horizontal-scroller/horizontal-scroller.ts
+++ src/resources/elements/horizontal-scroller/horizontal-scroller.ts
@@ -1,14 +1,29 @@
+import { autoinject } from "aurelia-framework";
+import { CssAnimator } from "aurelia-animator-css";
 import "./horizontal-scroller.scss";
 
+@autoinject
 export class HorizontalScroller {
+  constructor (
+    private animator: CssAnimator,
+  ) {}
 
   scroller: HTMLElement;
+  scrollPos: number;
 
   left(): void {
-    this.scroller.scrollLeft -= this.scroller.clientWidth * .9;
+    const scrollPos = (this.scrollPos ?? 0);
+    this.scroller.scroll({
+      left: scrollPos - Math.min(scrollPos, this.scroller.clientWidth * .45),
+      behavior: "smooth",
+    });
   }
 
   right(): void {
-    this.scroller.scrollLeft += this.scroller.clientWidth * .9;
+    const scrollPos = (this.scrollPos ?? 0);
+    this.scroller.scroll({
+      left: scrollPos + Math.min(this.scroller.clientWidth - scrollPos, this.scroller.clientWidth * .45),
+      behavior: "smooth",
+    });
   }
 }
