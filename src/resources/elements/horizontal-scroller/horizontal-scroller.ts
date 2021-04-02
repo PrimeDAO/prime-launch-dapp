fa2813dc4f97803dde3a6c14017ce6568650a40f
Doug Kent <github@douglaskent.com>
2021-04-01 19:00:40 -0600

    compute scroll distance by count of items

--- src/resources/elements/horizontal-scroller/horizontal-scroller.ts
+++ src/resources/elements/horizontal-scroller/horizontal-scroller.ts
@@ -1,6 +1,7 @@
 import { autoinject } from "aurelia-framework";
 import { CssAnimator } from "aurelia-animator-css";
 import "./horizontal-scroller.scss";
+import { bindable } from "aurelia-typed-observable-plugin";
 
 @autoinject
 export class HorizontalScroller {
@@ -10,20 +11,34 @@ export class HorizontalScroller {
 
   scroller: HTMLElement;
   scrollPos: number;
+  @bindable.number itemCount: number;
 
   left(): void {
+    const scrollDistance = this.scrollDistance();
     const scrollPos = (this.scrollPos ?? 0);
+
     this.scroller.scroll({
-      left: scrollPos - Math.min(scrollPos, this.scroller.clientWidth * .45),
+      left: scrollPos - Math.min(scrollPos, scrollDistance),
       behavior: "smooth",
     });
   }
 
   right(): void {
+
+    const scrollDistance = this.scrollDistance();
     const scrollPos = (this.scrollPos ?? 0);
+
     this.scroller.scroll({
-      left: scrollPos + Math.min(this.scroller.clientWidth - scrollPos, this.scroller.clientWidth * .45),
+      left: scrollPos + Math.min(this.scroller.scrollWidth - scrollPos, scrollDistance),
       behavior: "smooth",
     });
+  }
+
+  scrollDistance(): number {
+    const visibleWidth = this.scroller.clientWidth;
+    const itemWidth = this.scroller.scrollWidth / this.itemCount;
+    // itemWidth better not be 0
+    const visibleItemsCount = Math.floor(visibleWidth / itemWidth);
+    return itemWidth * visibleItemsCount;
   }
 }
