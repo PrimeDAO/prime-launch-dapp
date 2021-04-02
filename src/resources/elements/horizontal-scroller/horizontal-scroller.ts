d7bf017b734a0c42bcef3bc7b63b016b800f8720
Doug Kent <github@douglaskent.com>
2021-04-01 21:21:01 -0600

    scroller fully functional

--- src/resources/elements/horizontal-scroller/horizontal-scroller.ts
+++ src/resources/elements/horizontal-scroller/horizontal-scroller.ts
@@ -1,43 +1,62 @@
-import { autoinject } from "aurelia-framework";
-import { CssAnimator } from "aurelia-animator-css";
+import { autoinject, computedFrom } from "aurelia-framework";
 import "./horizontal-scroller.scss";
 import { bindable } from "aurelia-typed-observable-plugin";
 
 @autoinject
 export class HorizontalScroller {
-  constructor (
-    private animator: CssAnimator,
-  ) {}
 
   scroller: HTMLElement;
-  scrollPos: number;
+  scrollleft: number;
+
+  @computedFrom("scrollleft")
+  get scrollPos(): number { return this.scrollleft ?? 0; }
+
   @bindable.number itemCount: number;
 
   left(): void {
     const scrollDistance = this.scrollDistance();
-    const scrollPos = (this.scrollPos ?? 0);
 
     this.scroller.scroll({
-      left: scrollPos - Math.min(scrollPos, scrollDistance),
+      left: this.scrollPos - Math.min(this.scrollPos, scrollDistance),
       behavior: "smooth",
     });
   }
 
   right(): void {
     const scrollDistance = this.scrollDistance();
-    const scrollPos = (this.scrollPos ?? 0);
 
     this.scroller.scroll({
-      left: scrollPos + Math.min(this.scroller.scrollWidth - scrollPos, scrollDistance),
+      left: this.scrollPos + Math.min(this.scroller.scrollWidth - this.scrollPos, scrollDistance),
       behavior: "smooth",
     });
   }
 
+  itemWidth(): number {
+    return this.scroller.scrollWidth / this.itemCount;
+  }
+
   scrollDistance(): number {
     const visibleWidth = this.scroller.clientWidth;
-    const itemWidth = this.scroller.scrollWidth / this.itemCount;
     // itemWidth better not be 0
+    const itemWidth = this.itemWidth();
     const visibleItemsCount = Math.floor(visibleWidth / itemWidth);
+    /**
+     * scroll by the sum of the widths of the wholly-visible items
+     */
     return itemWidth * visibleItemsCount;
+  }
+
+  @computedFrom("scroller.scrollWidth", "scrollPos", "scroller.clientWidth")
+  get atEnd(): boolean {
+    /**
+     * if the distance between the scrollPos and the end of the scroller is <= the
+     * the width of the scroller, then presume we have scrolled as far as we can, or close enough.
+     */
+    return (this.scroller.scrollWidth - Math.ceil(this.scrollPos)) <= this.scroller.clientWidth;
+  }
+
+  @computedFrom("scrollPos")
+  get atBeginning(): boolean {
+    return this.scrollPos === 0;
   }
 }
