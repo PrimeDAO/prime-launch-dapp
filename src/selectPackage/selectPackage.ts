e95793d83c8dbd11d7d79eb32273517b4096761f
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-04-04 05:21:29 +0100

    Adjuted to requested changes

--- src/selectPackage/selectPackage.ts
+++ src/selectPackage/selectPackage.ts
@@ -2,25 +2,4 @@
 import "./selectPackage.scss";
 
 export class SelectPackage {
-  // Declare state variables
-  benefits: Array<string>;
-  discounts: { [plan:string]:{discount: number, benefits: Array<string>}};
-  constructor() {
-    // TODO: Network call to get up to date values
-    this.benefits=["support", "hosted"];
-    this.discounts={
-      "self-hosted": {
-        discount: 0,
-        benefits: ["support"],
-      },
-      "basic": {
-        discount: 1,
-        benefits: ["support"],
-      },
-      "prime": {
-        discount: 2,
-        benefits: ["support"],
-      },
-    };
-  }
 }
