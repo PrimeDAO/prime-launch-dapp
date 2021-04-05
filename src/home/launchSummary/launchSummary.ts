d48d80a95243481f6f7908e73274fce71508df3f
Doug Kent <github@douglaskent.com>
2021-04-05 16:33:55 -0600

    hot seeds

--- src/home/launchSummary/launchSummary.ts
+++ src/home/launchSummary/launchSummary.ts
@@ -1,7 +1,19 @@
+import { autoinject } from "aurelia-framework";
+import { Router } from "aurelia-router";
 import { bindable } from "aurelia-typed-observable-plugin";
+import { Address } from "services/EthereumService";
 import "./launchSummary.scss";
 
+@autoinject
 export class LaunchSummary {
 
-  @bindable.number i: number;
+  @bindable address: Address;
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  gotoDetails(): void {
+    this.router.navigateToRoute("seed", { address: this.address });
+  }
 }
--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,3 +1,7 @@
+import { bindable } from "aurelia-typed-observable-plugin";
+import { Address } from "services/EthereumService";
 import "./seed.scss";
+
 export class Seed {
+  @bindable address: Address;
 }
