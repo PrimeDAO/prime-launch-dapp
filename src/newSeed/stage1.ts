986caf87d099bec91126aa0c75f057e6099918cf
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-04-13 23:54:01 +0100

    Include URL checker during validation

--- src/newSeed/stage1.ts
+++ src/newSeed/stage1.ts
@@ -1,5 +1,6 @@
 import { BaseStage } from "./baseStage";
-
+// Let's import the utils helper
+import {Utils} from "../services/utils";
 export class Stage1 extends BaseStage {
   proceed(): void {
     let message: string;
@@ -9,12 +10,19 @@ export class Stage1 extends BaseStage {
     }
     else if (!this.seedConfig?.general?.projectWebsite) {
       message = "Plese enter the projec website";
-    } else if (!this.seedConfig?.general?.category) {
+    } else if (!Utils.isValidUrl(this.seedConfig?.general?.projectWebsite)) {
+      message = "Please enter a valid website url";
+    }
+    else if (!this.seedConfig?.general?.category) {
       message = "Enter a category";
     } else if (!this.seedConfig?.general?.whitepaper) {
       message = "We need your whitepaper address";
+    } else if (!Utils.isValidUrl(this.seedConfig?.general?.whitepaper)) {
+      message = "Please enter a valid website url";
     } else if (!this.seedConfig?.general?.github) {
       message = "Please put in a github link";
+    } else if (!Utils.isValidUrl(this.seedConfig?.general?.github)) {
+      message = "Please enter a valid website url";
     }
     if (message) {
       this.validationError(message);
