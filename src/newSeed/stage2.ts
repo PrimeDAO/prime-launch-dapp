ea8328ef70e05bbd7773008abf302a47e3bda143
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-04-14 14:11:23 +0100

    Remove all stages > 1

--- src/newSeed/stage2.ts
+++ src/newSeed/stage2.ts
@@ -1,22 +1,4 @@
 import { BaseStage } from "newSeed/baseStage";
 
 export class Stage2 extends BaseStage {
-  proceed(): void {
-    let message: string;
-
-    if (!this.seedConfig?.projectDetails?.summary) {
-      message = "Please Enter a summary of the Project";
-    }
-    else if (!this.seedConfig?.projectDetails?.proposition) {
-      message = "Please enter a propostion";
-    } else if (!this.seedConfig.projectDetails.category) {
-      message = "Please provide a category";
-    }
-
-    if (message) {
-      this.validationError(message);
-    } else {
-      this.next();
-    }
-  }
 }
