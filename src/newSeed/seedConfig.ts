f1b8f085547168a2c80348e2130787e6e11135cc
Kehinde Faleye <Kenny.fale.kf@gmail.com>
2021-04-14 17:21:59 +0100

    Stage 1 wizard component and validation (#7)
    * Stage 1 of wizrd component and validation
    
    * Stage 1 component and validation

--- src/newSeed/seedConfig.ts
+++ src/newSeed/seedConfig.ts
@@ -1,5 +1,31 @@
 export interface ISeedConfig {
+  general: {
+    projectName: string,
+    projectWebsite: string,
+    category: string,
+    whitepaper: string,
+    github: string,
+
+  },
+  projectDetails: {
+    summary: string,
+    proposition: string,
+    category: string
+  }
+
 }
 
 export class SeedConfig implements ISeedConfig {
+  general: {
+    projectName: string
+    projectWebsite: string,
+    category: string,
+    whitepaper: string,
+    github: string,
+  };
+  projectDetails: {
+    summary: string,
+    proposition: string,
+    category: string
+  }
 }
--- src/newSeed/stage1.ts
+++ src/newSeed/stage1.ts
@@ -1,5 +1,27 @@
-import { BaseStage } from "newSeed/baseStage";
-
+import { BaseStage } from "./baseStage";
+// Let's import the utils helper
+import {Utils} from "../services/utils";
 export class Stage1 extends BaseStage {
+  proceed(): void {
+    let message: string;
 
+    if (!this.seedConfig?.general?.projectName) {
+      message = "Enter a value for Project Name";
+    }
+    else if (!Utils.isValidUrl(this.seedConfig?.general?.projectWebsite, false)) {
+      message = "Please enter a valid website url";
+    }
+    else if (!this.seedConfig?.general?.category) {
+      message = "Enter a category";
+    } else if (!Utils.isValidUrl(this.seedConfig?.general?.whitepaper, false)) {
+      message = "Please enter a valid website url";
+    } else if (!Utils.isValidUrl(this.seedConfig?.general?.github, false)) {
+      message = "Please enter a valid website url";
+    }
+    if (message) {
+      this.validationError(message);
+    } else {
+      this.next();
+    }
+  }
 }
