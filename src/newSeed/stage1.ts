f9ddef0d005fcd0dcad4f38464e6c6fdab661b2d
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-04-14 17:44:34 +0100

    Changed font size to em

--- src/newSeed/stage1.ts
+++ src/newSeed/stage1.ts
@@ -6,17 +6,17 @@ export class Stage1 extends BaseStage {
     let message: string;
 
     if (!this.seedConfig?.general?.projectName) {
-      message = "Enter a value for Project Name";
+      message = "Please Enter a value for Project Name";
     }
     else if (!Utils.isValidUrl(this.seedConfig?.general?.projectWebsite, false)) {
-      message = "Please enter a valid website url";
+      message = "Please enter a valid url for project website";
     }
     else if (!this.seedConfig?.general?.category) {
       message = "Enter a category";
     } else if (!Utils.isValidUrl(this.seedConfig?.general?.whitepaper, false)) {
-      message = "Please enter a valid website url";
+      message = "Please enter a valid url for whitepaper";
     } else if (!Utils.isValidUrl(this.seedConfig?.general?.github, false)) {
-      message = "Please enter a valid website url";
+      message = "Please enter a valid url for github link";
     }
     if (message) {
       this.validationError(message);
