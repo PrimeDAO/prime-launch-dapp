1b6d6549cfda10f37a97982939e2d72dcd37d304
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-04-14 15:32:05 +0100

    No need to check for empty fields while checking valid Url

--- src/newSeed/stage1.ts
+++ src/newSeed/stage1.ts
@@ -8,19 +8,13 @@ export class Stage1 extends BaseStage {
     if (!this.seedConfig?.general?.projectName) {
       message = "Enter a value for Project Name";
     }
-    else if (!this.seedConfig?.general?.projectWebsite) {
-      message = "Plese enter the projec website";
-    } else if (!Utils.isValidUrl(this.seedConfig?.general?.projectWebsite, false)) {
+    else if (!Utils.isValidUrl(this.seedConfig?.general?.projectWebsite, false)) {
       message = "Please enter a valid website url";
     }
     else if (!this.seedConfig?.general?.category) {
       message = "Enter a category";
-    } else if (!this.seedConfig?.general?.whitepaper) {
-      message = "We need your whitepaper address";
     } else if (!Utils.isValidUrl(this.seedConfig?.general?.whitepaper, false)) {
       message = "Please enter a valid website url";
-    } else if (!this.seedConfig?.general?.github) {
-      message = "Please put in a github link";
     } else if (!Utils.isValidUrl(this.seedConfig?.general?.github, false)) {
       message = "Please enter a valid website url";
     }
