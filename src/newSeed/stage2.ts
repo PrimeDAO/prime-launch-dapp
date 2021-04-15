3babc7b940cd6baf88dd68650419f24c2a2bef36
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-04-15 01:33:18 +0100

    Added validation to the stage 2 component of wizard

--- src/newSeed/stage2.ts
+++ src/newSeed/stage2.ts
@@ -1,5 +1,20 @@
 import { BaseStage } from "newSeed/baseStage";
 
 export class Stage2 extends BaseStage {
-
+    proceed(): void {
+        let message: string;
+    
+        if (!this.seedConfig?.projectDetails?.summary) {
+          message = "Please fill the summary of the project";
+        } else if (!this.seedConfig?.projectDetails?.proposition) {
+            message = "Please fill the value proposition";
+        } else if (!this.seedConfig?.projectDetails?.category) {
+            message = "Please fill the category of the project";
+        }
+        if (message) {
+          this.validationError(message);
+        } else {
+          this.next();
+        }
+      }
 }
