a8b79ef47cd62a769e9e4fca1c36c5c6df78ab46
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-05-06 07:00:06 +0100

    Make stage 4 comp look more like the mock

--- src/newSeed/stage4.ts
+++ src/newSeed/stage4.ts
@@ -1,5 +1,9 @@
 import { BaseStage } from "newSeed/baseStage";
 
 export class Stage4 extends BaseStage {
-
+  readFile(event:any): void {
+    if (event.target.files[0]) {
+      this.seedConfig.seedDetails.whitelist.whitelistFile = event.target.files[0];
+    }
+  }
 }
