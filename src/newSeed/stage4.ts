ad13c50be426b065c5c6fa910017b4487d44ea13
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-06-03 19:16:11 +0100

    Use BigNumber for comparing large integers because BigNumbers might overflow javascript numbers

--- src/newSeed/stage4.ts
+++ src/newSeed/stage4.ts
@@ -6,6 +6,7 @@ import { BaseStage } from "newSeed/baseStage";
 import Litepicker from "litepicker";
 import { Utils } from "services/utils";
 import { EventAggregator } from "aurelia-event-aggregator";
+import { BigNumber } from "ethers";
 
 @autoinject
 export class Stage4 extends BaseStage {
@@ -91,8 +92,8 @@ export class Stage4 extends BaseStage {
       message = "Please enter a non-zero value for the Funding Target";
     } else if (!this.seedConfig.seedDetails.fundingMax || this.seedConfig.seedDetails.fundingMax === "0") {
       message = "Please enter a non-zero number for the Funding Max";
-    } else if (Number.parseInt(this.seedConfig.seedDetails.fundingTarget) > Number.parseInt(this.seedConfig.seedDetails.fundingMax)) {
-      message = "Please enter a value for Funding Target smaller Funding Max";
+    } else if (BigNumber.from(this.seedConfig.seedDetails.fundingTarget).gt(this.seedConfig.seedDetails.fundingMax)) {
+      message = "Please enter a value for Funding Target smaller than Funding Max";
     } else if (!this.seedConfig.seedDetails.vestingDays || this.seedConfig.seedDetails.vestingDays <= 0) {
       message = "Please enter a non-zero value for  \"Seed tokens vested for\" ";
     } else if (!this.seedConfig.seedDetails.vestingCliff || this.seedConfig.seedDetails.vestingCliff <= 0) {
