5109509a61bc544cf36a41df7a90fdaa697c11ad
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-06-03 05:41:52 +0100

    Make sure the vesting cliff is less than the vesting dates

--- src/newSeed/stage4.ts
+++ src/newSeed/stage4.ts
@@ -91,11 +91,11 @@ export class Stage4 extends BaseStage {
       message = "Please enter a non-zero value for the Funding Target";
     } else if (!this.seedConfig.seedDetails.fundingMax || this.seedConfig.seedDetails.fundingMax === "0") {
       message = "Please enter a non-zero number for the Funding Max";
-    } else if (!this.seedConfig.seedDetails.vestingDays || this.seedConfig.seedDetails.vestingDays <= 0) {
+    } else if (!this.seedConfig.seedDetails.vestingDays || this.seedConfig.seedDetails.vestingDays === "0") {
       message = "Please enter a non-zero value for  \"Seed tokens vested for\" ";
-    } else if (!this.seedConfig.seedDetails.vestingCliff || this.seedConfig.seedDetails.vestingCliff <= 0) {
+    } else if (!this.seedConfig.seedDetails.vestingCliff || this.seedConfig.seedDetails.vestingCliff === "0") {
       message = "Please enter a non-zero value for \"with a cliff of\" ";
-    } else if (!(this.seedConfig.seedDetails.vestingCliff < this.seedConfig.seedDetails.vestingDays)) {
+    } else if (Number.parseInt(this.seedConfig.seedDetails.vestingCliff) > Number.parseInt(this.seedConfig.seedDetails.vestingDays)) {
       message = "Plese enter a value of \"with a cliff of\" lesser than \"Seed tokens vested for \"";
     } else if (!this.startDate) {
       message = "Please select a Start Date";
