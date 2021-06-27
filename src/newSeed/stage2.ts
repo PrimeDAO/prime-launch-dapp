0140ef9844794ac41f38fbc00d0d76edb20692d1
Doug Kent <github@douglaskent.com>
2021-06-27 16:42:20 -0600

    use img tag to validate, redo ux

--- src/newSeed/stage2.ts
+++ src/newSeed/stage2.ts
@@ -1,28 +1,34 @@
-import { bindable } from "aurelia-typed-observable-plugin";
-import axios from "axios";
 import { BaseStage } from "newSeed/baseStage";
 import { Utils } from "services/utils";
 
 export class Stage2 extends BaseStage {
-  private isValidFile(file: string): boolean {
+
+  private isValidImageFormat(file: string): boolean {
     const re = /(\.jpg|\.bmp|\.gif|\.png)$/i;
     return re.test(String(file).toLowerCase());
   }
 
-  @bindable isLoadable = false;
+  seedLogoIsValid = false;
+  seedLogoIsLoaded = false;
+  private isValidSeedLogo(): string {
+    let message;
 
-  private isLinkNotBroken(url:string):void {
-
-    if (!url || url.length < 4 || !this.isValidFile(url)) {
-      this.isLoadable = false;
+    if (!Utils.isValidUrl(encodeURI(this.seedConfig.projectDetails.logo))) {
+      message = "Please enter a valid URL for Project Logo";
+    } else if (!this.isValidImageFormat(this.seedConfig.projectDetails.logo)) {
+      message = "Please supply a valid image file type for Project Logo";
     }
+    this.seedLogoIsValid = !message;
+    if (!this.seedLogoIsValid) {
+      this.seedLogoIsLoaded = false;
+    }
+    return message;
+  }
 
-    axios.head(url)
-      .then(res => {
-        this.isLoadable = res.status === 200;
-      }).catch( _ => {
-        this.isLoadable = false;
-      });
+  private isLoadedSeedLogo(valid: boolean): void {
+    if (this.seedLogoIsValid) {
+      this.seedLogoIsLoaded = valid;
+    }
   }
 
   async proceed(): Promise<void> {
@@ -42,13 +48,10 @@ export class Stage2 extends BaseStage {
       message = "Please enter a value for Value Proposition";
     } else if (!this.seedConfig.projectDetails.teamDescription) {
       message = "Please enter a value for Team Description";
-    } else if (!Utils.isValidUrl(encodeURI(this.seedConfig.projectDetails.logo))) {
-      message = "Please enter a valid URL for Project Logo";
-    } else if (!this.isValidFile(this.seedConfig.projectDetails.logo)) {
-      message = "Please supply a valid image file type for Project Logo";
-    } else if (!this.isLoadable) {
-      message = "No image found under the provided Project Logo URL";
+    } else if (!this.seedLogoIsLoaded) {
+      message = "No valid image found at the provided Project Logo URL";
     }
+
     this.stageState.verified = !message;
     return Promise.resolve(message);
   }
