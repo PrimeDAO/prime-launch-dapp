19a52402f51e8034d534df6660e5dd0495099c55
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-06-29 21:09:55 +0100

    Call the clearstate function in the constructor of the seedConfig class rather than have duplicate code

--- src/newSeed/seedConfig.ts
+++ src/newSeed/seedConfig.ts
@@ -82,24 +82,16 @@ export interface ISeedConfig {
 }
 
 export class SeedConfig implements ISeedConfig {
-  public version = "1.0.0";
-  public general = {
-    customLinks: [],
-  } as IGeneral;
-  public projectDetails = {
-    summary: "",
-    proposition: "",
-    teamDescription: "",
-  } as IProjectDetails;
-  public tokenDetails = {
-    tokenDistrib: [],
-  } as ITokenDetails;
-  public contactDetails={
-    remarks: "",
-  } as IContactDetails;
-  public seedDetails = {
-    geoBlock: false,
-  } as ISeedDetails;
+  public version: string;
+  public general: IGeneral;
+  public projectDetails:IProjectDetails;
+  public tokenDetails: ITokenDetails;
+  public contactDetails: IContactDetails;
+  public seedDetails: ISeedDetails;
+
+  constructor() {
+    this.clearState();
+  }
 
   clearState(): void {
     this.general = {
