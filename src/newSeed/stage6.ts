9eed36cb7cbf354a2ad557dbd3d18f4abe781d68
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-07-01 02:31:32 +0100

    Format the vesting dates to days, minutes and seconds and display project logo instead of the link

--- src/newSeed/stage6.ts
+++ src/newSeed/stage6.ts
@@ -1,4 +1,23 @@
+import { EventAggregator } from "aurelia-event-aggregator";
+import { Router } from "aurelia-router";
+import { autoinject } from "aurelia-framework";
+import { computedFrom } from "aurelia-framework";
+import { DateService } from "./../services/DateService";
 import { BaseStage } from "newSeed/baseStage";
+import { TimespanResolution } from "./../services/DateService";
 
+@autoinject
 export class Stage6 extends BaseStage {
+  constructor(private dateService: DateService, router: Router, eventAggregator: EventAggregator) {
+    super(router, eventAggregator);
+  }
+
+  @computedFrom("seedConfig.seedDetails.vestingDays")
+  get vestingDays(): string {
+    return this.dateService.ticksToTimeSpanString(this.seedConfig.seedDetails.vestingDays * 1000, TimespanResolution.minutes);
+  }
+  @computedFrom("seedConfig.seedDetails.vestingCliff")
+  get vestingCliff(): string {
+    return this.dateService.ticksToTimeSpanString(this.seedConfig.seedDetails.vestingCliff * 1000, TimespanResolution.minutes);
+  }
 }
