37cb0a5cedd9b067ddab21ce38070910f020ad74
Doug Kent <github@douglaskent.com>
2021-07-07 14:46:59 -0600

    remove big gap in seed dashboard layout

---
+++ src/seedDashboard/seedAbout.ts
@@ -0,0 +1,26 @@
+import { containerless } from "aurelia-framework";
+import { bindable } from "aurelia-typed-observable-plugin";
+import { Seed } from "entities/Seed";
+
+@containerless
+export class SeedAbout{
+
+  @bindable seed: Seed;
+  @bindable type: string;
+
+  linkIcons = new Map<string, string>([
+    ["twitter", "fab fa-twitter"],
+    ["telegram", "fab fa-telegram-plane"],
+    ["discord", "fab fa-discord"],
+    ["medium", "fab fa-medium-m"],
+    ["github", "fab fa-github"],
+    ["website", "fa fa-globe-americas"],
+    ["misc", "fa fa-external-link-alt"],
+    ["home", "fas fa-home"],
+    ["pdf", "fas fa-file-pdf"],
+  ]);
+
+  iconClassForLinkType(type: string): string {
+    return this.linkIcons.get(type.toLowerCase()) ?? this.linkIcons.get("misc");
+  }
+}
--- src/seedDashboard/seedDashboard.ts
+++ src/seedDashboard/seedDashboard.ts
@@ -165,22 +165,6 @@ export class SeedDashboard {
     }
   }
 
-  linkIcons = new Map<string, string>([
-    ["twitter", "fab fa-twitter"],
-    ["telegram", "fab fa-telegram-plane"],
-    ["discord", "fab fa-discord"],
-    ["medium", "fab fa-medium-m"],
-    ["github", "fab fa-github"],
-    ["website", "fa fa-globe-americas"],
-    ["misc", "fa fa-external-link-alt"],
-    ["home", "fas fa-home"],
-    ["pdf", "fas fa-file-pdf"],
-  ]);
-
-  iconClassForLinkType(type: string): string {
-    return this.linkIcons.get(type.toLowerCase()) ?? this.linkIcons.get("misc");
-  }
-
   connect(): void {
     this.ethereumService.ensureConnected();
   }
