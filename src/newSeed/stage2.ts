e586dcaf9beacdb7e644bcee0dcdc121c1242848
Doug Kent <github@douglaskent.com>
2021-08-10 13:26:42 -0700

    rename Project Summary and correct tooltips

--- src/newSeed/stage2.ts
+++ src/newSeed/stage2.ts
@@ -34,7 +34,7 @@ export class Stage2 extends BaseStage {
   validateInputs(): Promise<string> {
     let message: string;
     if (!this.seedConfig.projectDetails.summary) {
-      message = "Please enter a value for Project Summary";
+      message = "Please enter a value for Short Description";
     } else if (!this.seedConfig.projectDetails.proposition) {
       message = "Please enter a value for Value Proposition";
     } else if (!this.seedConfig.projectDetails.teamDescription) {
