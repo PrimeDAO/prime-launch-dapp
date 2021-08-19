ccb59fe5487c08a0a7f6d35d419f34bab4ba981e
Kehinde Faleye <Kenny.fale.kf@gmail.com>
2021-08-19 20:35:19 +0100

    Funding input rework (#137)
    * Stage 1 of wizrd component and validation
    
    * Make this commit differ from master commit
    
    * Place the funding target on top of the funding max and not beside it
    
    * Adjust heirarchy of DOM components
    
    * Display the funcing symbol and icon next to funding target and funding max on stage 4 of seed reg wizard
    
    * Add soace between the subtext and the input and padd the subtext
    
    * Change the funding max label to maximum
    
    * Rename max to maximum and display the token icon on the summary page
    
    * Change all max to maximum (Rewording)
    
    * Add a bottom margin to the label in mobile layout
    
    * Remove the bottom margin from labels with tooltips

--- src/newSeed/stage4.ts
+++ src/newSeed/stage4.ts
@@ -108,11 +108,11 @@ export class Stage4 extends BaseStage {
     } else if (!this.seedConfig.seedDetails.fundingTarget || this.seedConfig.seedDetails.fundingTarget === "0") {
       message = "Please enter a number greater than zero for the Funding Target";
     } else if (!this.seedConfig.seedDetails.fundingMax || this.seedConfig.seedDetails.fundingMax === "0") {
-      message = "Please enter a number greater than zero for the Funding Max";
+      message = "Please enter a number greater than zero for the Funding Maximum";
     } else if (BigNumber.from(this.seedConfig.seedDetails.fundingTarget).gt(this.seedConfig.seedDetails.fundingMax)) {
-      message = "Please enter a value for Funding Target less than or equal to Funding Max";
+      message = "Please enter a value for Funding Target less than or equal to Funding Maximum";
     } else if (this.seedConfig.tokenDetails.maxSeedSupply && this.numberService.fromString(fromWei(this.seedConfig.seedDetails.fundingMax)) > this.numberService.fromString(fromWei(this.seedConfig.tokenDetails.maxSeedSupply)) * this.numberService.fromString(fromWei(this.seedConfig.seedDetails.pricePerToken))) {
-      message = "Funding Max cannot be greater than Maximum Project Token Supply times the Funding Tokens per Project Token";
+      message = "Funding Maximum cannot be greater than Maximum Project Token Supply times the Funding Tokens per Project Token";
     } else if (!(this.seedConfig.seedDetails.vestingPeriod >= 0)) {
       message = "Please enter a number greater than zero for  \"Project tokens vested for\" ";
     } else if (!(this.seedConfig.seedDetails.vestingCliff >= 0)) {
