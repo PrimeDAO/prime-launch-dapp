bfc9cec34b9ebf6bef915e61b04120449dc540b4
Doug Kent <github@douglaskent.com>
2021-09-06 11:51:06 -0600

    subscribe button

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,18 +1,22 @@
+import { EventAggregator } from "aurelia-event-aggregator";
 import { SeedService } from "services/SeedService";
 import { autoinject, singleton } from "aurelia-framework";
 import { Router } from "aurelia-router";
 import "./home.scss";
 import { Seed } from "entities/Seed";
+import { Utils } from "services/utils";
 
 @singleton(false)
 @autoinject
 export class Home {
 
   featuredSeeds: Array<Seed> = null;
+  subscriberEmail: string;
 
   constructor(
     private router: Router,
     private seedService: SeedService,
+    private eventAggregator: EventAggregator,
   ) {
   }
 
@@ -25,4 +29,11 @@ export class Home {
   navigate(href: string): void {
     this.router.navigate(href);
   }
-}
+
+  subscribe(): void {
+    if (!Utils.isValidEmail(this.subscriberEmail)) {
+      this.eventAggregator.publish("handleValidationError", "Please enter a valid email address");
+    } else {
+      // window.open(`mailto:renc@curvelabs.eu?subject=Sign-up%20for%20Prime%20Launch%20Newsletter&body=My%20email%20address: ${this.subscriberEmail}`, "#", "noopener noreferrer");
+    }
+  }}
