5e8cd23524dea81d396180d7d3973311a65ed7ed
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2021-09-30 09:11:34 +0200

    cleanup for out of scope classes

--- src/newLaunch/lbp/stage7.ts
+++ src/newLaunch/lbp/stage7.ts
@@ -2,11 +2,8 @@ import { EthereumService } from "../../services/EthereumService";
 import { autoinject, computedFrom } from "aurelia-framework";
 import { BaseStage } from "newLaunch/baseStage";
 import { Router, RouteConfig, Redirect } from "aurelia-router";
-import { LbpService } from "services/LbpService";
+// import { LbpService } from "services/LbpService";
 import { EventAggregator } from "aurelia-event-aggregator";
-import { EventConfigException } from "services/GeneralEvents";
-// import { fromWei } from "services/EthereumService";
-import { NumberService } from "services/NumberService";
 import { TokenService } from "services/TokenService";
 
 @autoinject
@@ -15,10 +12,10 @@ export class Stage7 extends BaseStage {
   constructor(
     router: Router,
     eventAggregator: EventAggregator,
-    private lbpService: LbpService,
+    // private lbpService: LbpService,
     private ethereumService: EthereumService,
     tokenService: TokenService,
-    private numberService: NumberService) {
+  ) {
     super(router, eventAggregator, tokenService);
   }
 
@@ -26,8 +23,8 @@ export class Stage7 extends BaseStage {
     /**
      * heuristic for whether we have data.  Possibility is that the user
      * has used the 'back' button or otherwize figured out how to return
-     * to this page, for example, just after having submitting a Lbp,
-     * where the lbpConfig will have been deleted.
+     * to this page, for example, just after having submitting a Seed,
+     * where the seedConfig will have been deleted.
      */
     if (!routeConfig.settings.lbpConfig.general.projectName?.length) {
       return new Redirect("");
@@ -37,34 +34,12 @@ export class Stage7 extends BaseStage {
   }
 
   attached(): void {
-    // this.lbpConfig.lbpDetails.fundingMax = toWei("100").toString();
-    // this.lbpConfig.lbpDetails.pricePerToken = toWei(".5").toString();
-    // this.wizardState.projectTokenSymbol = "PRIME";
-    // const distributableLbps = this.numberService.fromString(fromWei(this.lbpConfig.lbpDetails.fundingMax))
-    //   / this.numberService.fromString(fromWei(this.lbpConfig.lbpDetails.pricePerToken));
-    // this.wizardState.requiredLbpFee = distributableLbps * this.lbpFee;
-    // this.wizardState.requiredLbpDeposit = distributableLbps + this.wizardState.requiredLbpFee;
+    // ToDo
   }
 
   async submit(): Promise<void> {
-    try {
-      this.eventAggregator.publish("lbp.creating", true);
-      this.wizardState.lbpHash = await this.lbpService.deployLbp(this.lbpConfig);
-      if (this.wizardState.lbpHash) {
-      // this.eventAggregator.publish("handleInfo", `Successfully pinned LBP registration hash at: this.ipfsService.getIpfsUrl(this.lbpHash)`);
-        this.lbpConfig.clearState();
-        for (let i = 1; i <= this.maxStage; ++i) {
-          this.stageStates[i].verified = false;
-        }
-        this.eventAggregator.publish("lbp.clearState", true);
-        this.next();
-      }
-    } catch (ex) {
-      this.eventAggregator.publish("handleException", new EventConfigException("Sorry, an error occurred", ex));
-    }
-    finally {
-      this.eventAggregator.publish("lbp.creating", false);
-    }
+    // ToDo
+    this.next();
   }
 
   @computedFrom("ethereumService.defaultAccountAddress")
--- src/newLaunch/lbp/stage8.ts
+++ src/newLaunch/lbp/stage8.ts
@@ -1,9 +1,9 @@
 import { Router } from "aurelia-router";
 import { EventAggregator } from "aurelia-event-aggregator";
-import { autoinject, computedFrom } from "aurelia-framework";
+import { autoinject /* , computedFrom */ } from "aurelia-framework";
 import { BaseStage } from "newLaunch/baseStage";
-import { IpfsService } from "services/IpfsService";
 import { TokenService } from "services/TokenService";
+// import { IpfsService } from "services/IpfsService";
 
 @autoinject
 export class Stage8 extends BaseStage {
@@ -11,12 +11,13 @@ export class Stage8 extends BaseStage {
     router: Router,
     eventAggregator: EventAggregator,
     tokenService: TokenService,
-    private ipfsService: IpfsService) {
+    // private ipfsService: IpfsService,
+  ) {
     super(router, eventAggregator, tokenService);
   }
 
-  @computedFrom("wizardState.lbpHash")
-  get ipfsURL(): string {
-    return this.ipfsService.getIpfsUrl(this.wizardState.lbpHash);
-  }
+  // @computedFrom("wizardState.lbpHash")
+  // get ipfsURL(): string {
+  //   return this.ipfsService.getIpfsUrl(this.wizardState.lbpHash);
+  // }
 }
