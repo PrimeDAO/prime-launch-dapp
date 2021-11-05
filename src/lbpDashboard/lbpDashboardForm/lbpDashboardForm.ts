29e4c021cb6cfc0ba1ff81c239197607df00c737
Doug Kent <github@douglaskent.com>
2021-11-05 07:47:00 +0000

    fix exchange price in dashboard form

--- src/lbpDashboard/lbpDashboardForm/lbpDashboardForm.ts
+++ src/lbpDashboard/lbpDashboardForm/lbpDashboardForm.ts
@@ -31,18 +31,6 @@ export class lbpDashboardForm {
   sorSwapInfo: SwapInfo;
   userFundingTokenAllowance: BigNumber;
 
-  // @computedFrom("amountToPay", "lbpManager.projectTokensPerFundingToken")
-  // get projectTokensToPurchase(): BigNumber {
-  //   // if (this.amountToPay) {
-  //   const projectTokens = this.numberService.fromString(fromWei(this.amountToPay, this.lbpManager.fundingTokenInfo.decimals).toString())
-  //   * this.lbpManager.projectTokensPerFundingToken;
-
-  //   return toWei(projectTokens, this.lbpManager.projectTokenInfo.decimals);
-  // } else {
-  //   return BigNumber.from(0);
-  // }
-  // }
-
   get priceImpact(): number {
     return 0;
   }
@@ -74,6 +62,17 @@ export class lbpDashboardForm {
   @computedFrom("lbpManager.userHydrated", "ethereumService.defaultAccountAddress")
   get connected(): boolean {
     return !!this.ethereumService.defaultAccountAddress && this.lbpManager?.userHydrated;
+  }
+
+  @computedFrom("projectTokensToPurchase", "fundingTokensToPay")
+  get projectTokensPerFundingToken(): number {
+    if (!this.fundingTokensToPay?.gt(0) || !this.projectTokensToPurchase?.gt(0)) {
+      return 0;
+    }
+
+    return this.numberService.fromString(fromWei(this.fundingTokensToPay).toString())
+      /
+      this.numberService.fromString(fromWei(this.projectTokensToPurchase).toString());
   }
 
   private fundingTokensToPayChanged(): void {
@@ -120,21 +119,23 @@ export class lbpDashboardForm {
     if (this.fundingTokensToPay?.gt(0)) {
       try {
         if (this.fundingTokenInfo.address !== this.lbpManager.fundingTokenAddress) {
-          this.sorSwapInfo =
-            await this.balancerService.getSwapFromSor(
-              this.fundingTokensToPay,
-              this.fundingTokenInfo,
-              this.lbpManager.projectTokenInfo) as SwapInfo;
+
+          this.sorSwapInfo = await this.balancerService.getSwapFromSor(
+            this.fundingTokensToPay,
+            this.fundingTokenInfo,
+            this.lbpManager.projectTokenInfo) as SwapInfo;
+
           this.projectTokensToPurchase = this.sorSwapInfo.returnAmount;
-        } else {
+
+        } else { // using the pool funding token
+
           this.sorSwapInfo= null;
-          if (this.fundingTokensToPay?.gt(0)) {
-            const projectTokens = this.numberService.fromString(fromWei(this.fundingTokensToPay, this.lbpManager.fundingTokenInfo.decimals).toString())
-              * this.lbpManager.projectTokensPerFundingToken;
-            this.projectTokensToPurchase = toWei(projectTokens, this.lbpManager.projectTokenInfo.decimals);
-          } else {
-            this.projectTokensToPurchase = BigNumber.from(0);
-          }
+
+          const projectTokens = this.numberService.fromString(fromWei(this.fundingTokensToPay, this.lbpManager.fundingTokenInfo.decimals).toString())
+            * this.lbpManager.projectTokensPerFundingToken;
+
+          this.projectTokensToPurchase = toWei(projectTokens, this.lbpManager.projectTokenInfo.decimals);
+
         }
       } catch (ex) {
         this.eventAggregator.publish("handleException", new EventConfigException("Sorry, an error occurred", ex));
