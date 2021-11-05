b31aed475f08f733fd4cb41eda617e7fb5acf94c
Gilad <gilad@tsabar.net>
2021-11-05 13:37:58 +0100

    updated injections

--- src/services/ProjectTokenHistoricalPriceService.ts
+++ src/services/ProjectTokenHistoricalPriceService.ts
@@ -1,11 +1,11 @@
 import { EthereumService, Networks } from "services/EthereumService";
-import { Lbp } from "entities/Lbp";
 
 import { DateService } from "services/DateService";
 import { NumberService } from "services/NumberService";
 import { TokenService } from "services/TokenService";
-import { ITokenInfo } from "services/TokenTypes";
+import { LbpManager } from "entities/LbpManager";
 import { LbpProjectTokenPriceService } from "./LbpProjectTokenPriceService";
+import { Lbp } from "entities/Lbp";
 
 import { jsonToGraphQLQuery } from "json-to-graphql-query";
 import axios from "axios";
@@ -23,20 +23,16 @@ interface ISwapRecord {
 interface IHistoricalPriceRecord { time: number, value?: number }
 
 @autoinject
-export class GetProjectTokenHistoricalPriceService {
+export class ProjectTokenHistoricalPriceService {
   private historicalPrices = new Array<IHistoricalPriceRecord>();
-  private dateService = new DateService();
-  private tokenService: TokenService;
-  private lbpProjectTokenPriceService = new LbpProjectTokenPriceService();
 
   constructor(
+    private dateService: DateService,
+    private tokenService: TokenService,
+    private lbpProjectTokenPriceService: LbpProjectTokenPriceService,
     private ethereumService: EthereumService,
-    private lbp: Lbp,
     private numberService: NumberService,
-    private address: string,
-    private fundingTokenInfo: ITokenInfo,
   ) {
-    this.address = lbp.poolId.toLowerCase();
   }
 
   private getBalancerSubgraphUrl(): string {
@@ -47,12 +43,12 @@ export class GetProjectTokenHistoricalPriceService {
     return `https://api.coingecko.com/api/v3/coins/${fundingTokenId}/market_chart/range?vs_currency=usd&from=${startTime}&to=${endTime}`;
   }
 
-  public async getPricesHistory(startTime: Date): Promise<Array<IHistoricalPriceRecord>> {
-    if (!this.lbp || !this.lbp.poolId) {
+  public async getPricesHistory(lbpMgr: LbpManager): Promise<Array<IHistoricalPriceRecord>> {
+    if (!lbpMgr.lbp || !lbpMgr.lbp.poolId) {
       return [];
     }
 
-    const startingSeconds = startTime.getTime() / 1000;
+    const startingSeconds = lbpMgr.startTime.getTime() / 1000;
     const hourSeconds = 60/*min*/ * 60/*sec*/;
 
     /* Rounded to the nearest hour */
@@ -73,7 +69,7 @@ export class GetProjectTokenHistoricalPriceService {
        */
       const endDateSeconds = endTimeSeconds;
       // const endDateSeconds = swaps.length ? swaps[swaps.length-1].timestamp : endTimeSeconds;
-      fetched = await this.fetchSwaps(endDateSeconds, startingSeconds);
+      fetched = await this.fetchSwaps(endDateSeconds, startingSeconds, lbpMgr.lbp);
       swaps = swaps.concat(fetched);
     } while (fetched.length === 1000);
 
@@ -84,13 +80,14 @@ export class GetProjectTokenHistoricalPriceService {
 
       const prices = await axios.get(
         this.getCoingeckoUrl(
-          this.fundingTokenInfo.id,
+          lbpMgr.fundingTokenInfo.id,
           swaps[0].timestamp,
           endTimeSeconds,
         ),
       );
       const fundingTokenPricesUSD = await prices.data.prices.map(price => {
         return {
+          // price: [time, value]
           time: Math.floor(price[0] / (hourSeconds * 1000)) * (hourSeconds),
           value: price[1],
         };
@@ -100,33 +97,6 @@ export class GetProjectTokenHistoricalPriceService {
       /**
        * enumerate every day
        */
-
-      // let latestPrice = 0;
-      // let swapIndex = 0;
-      // const amounts = {
-      //   in: 0,
-      //   out: 0,
-      // };
-
-      // fundingTokenPricesUSD.forEach(price => {
-
-      //   console.log({swapIndex}, {price});
-      //   if (swapIndex <= swaps.length - 1 && price.time >= swaps[swapIndex].timestamp) {
-      //     amounts.in = parseFloat(swaps[swapIndex].tokenAmountIn);
-      //     amounts.out = parseFloat(swaps[swapIndex].tokenAmountOut);
-      //     console.log(new Date(swaps[swapIndex].timestamp * 1000), {amounts});
-
-      //     swapIndex++;
-      //   }
-
-      //   latestPrice = this.lbpProjectTokenPriceService.getProjectPriceRatio(
-      //     amounts.in, // funding token (e.g. usdc)
-      //     amounts.out, // project token (e.g. prime)
-      //     0.5, // funding token price
-      //   ) * price.value;
-
-      //   price.value = latestPrice;
-      // });
 
       for (let timestamp = Math.floor(swaps[0].timestamp / hourSeconds ) * hourSeconds; timestamp <= endTimeSeconds; timestamp += hourSeconds) {
 
@@ -150,26 +120,12 @@ export class GetProjectTokenHistoricalPriceService {
           }
         }
 
-        // let liquidityCurrent: number;
-        // let liquidityLast: number;
-        // let timeSpan: number;
         if (todaysSwaps?.length) {
-          // if (todaysSwaps.length > 1) {
-          //   liquidityCurrent = this.numberService.fromString(todaysSwaps[todaysSwaps.length-1].tokenAmountIn) / this.numberService.fromString(todaysSwaps[todaysSwaps.length-1].tokenAmountOut) * fundingTokenPricesUSD[fundingTokenPricesUSD.length - 1].value; //USD Price
-          //   liquidityLast = this.numberService.fromString(todaysSwaps[todaysSwaps.length-2].tokenAmountIn) / this.numberService.fromString(todaysSwaps[todaysSwaps.length-2].tokenAmountOut) * fundingTokenPricesUSD[fundingTokenPricesUSD.length - 2].value; //USD Price
-          //   timeSpan = todaysSwaps[todaysSwaps.length-1].timestamp - todaysSwaps[todaysSwaps.length-2].timestamp / 60 / 60;
-          // }
-          // average
-          // const liquidityThisDay = todaysSwaps.reduce((accumulator, currentValue) =>
-          //   accumulator + this.numberService.fromString(currentValue.poolLiquidity), 0) / todaysSwaps.length;
-
-          // max
-          // const liquidityThisDay = todaysSwaps.reduce((accumulator, currentValue) =>
-          //   accumulator = Math.floor(accumulator, this.numberService.fromString(currentValue.poolLiquidity)), 0);
-
-          // closing
-
-          const liquidityThisDay = this.numberService.fromString(todaysSwaps[todaysSwaps.length-1].tokenAmountIn) / this.numberService.fromString(todaysSwaps[todaysSwaps.length-1].tokenAmountOut) * fundingTokenPricesUSD[fundingTokenPricesUSD.length - 1].value; //USD Price
+          const liquidityThisDay = (
+            this.numberService.fromString(todaysSwaps[todaysSwaps.length-1].tokenAmountIn) /
+            this.numberService.fromString(todaysSwaps[todaysSwaps.length-1].tokenAmountOut) *
+            fundingTokenPricesUSD[fundingTokenPricesUSD.length - 1].value //USD Price
+          );
 
           returnArray.push({
             time: timestamp,
@@ -190,13 +146,12 @@ export class GetProjectTokenHistoricalPriceService {
           });
         }
       }
-      // returnArray.push(...fundingTokenPricesUSD);
     }
 
     return returnArray;
   }
 
-  private fetchSwaps(endDateSeconds: number, startDateSeconds: number): Promise<Array<ISwapRecord>> {
+  private fetchSwaps(endDateSeconds: number, startDateSeconds: number, lbp: Lbp): Promise<Array<ISwapRecord>> {
     const uri = this.getBalancerSubgraphUrl();
     const query = {
       swaps: {
@@ -205,7 +160,7 @@ export class GetProjectTokenHistoricalPriceService {
           orderBy: "timestamp",
           orderDirection: "desc",
           where: {
-            poolId: this.lbp.poolId.toLowerCase(),
+            poolId: lbp.poolId.toLowerCase(),
             timestamp_gte: startDateSeconds,
             timestamp_lte: endDateSeconds,
           },
@@ -236,8 +191,7 @@ export class GetProjectTokenHistoricalPriceService {
         //   new EventConfigFailure(`Pool: Error fetching market cap history: ${error?.response?.data?.error?.message ?? error?.message}`));
         console.log({
           fetchSwapsError: error.message,
-          address: this.address,
-          id: this.lbp.poolId,
+          id: lbp.poolId,
         });
 
         // throw new Error(`${error.response?.data?.error.message ?? "Error fetching token info"}`);
