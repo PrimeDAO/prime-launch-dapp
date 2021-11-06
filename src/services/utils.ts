3daabd56fb82003fedeae4cccd16266c224f2567
Doug Kent <github@douglaskent.com>
2021-11-06 08:55:33 +0000

    fix waitUntilTrue

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -85,7 +85,7 @@ export class Utils {
   public static waitUntilTrue(test: () => Promise<boolean> | boolean, timeOut = 1000): Promise<void> {
     return new Promise((resolve, reject) => {
       const timerId = setInterval(async () => {
-        if (await test()) { return resolve(); }
+        if (await test()) { clearTimeout(timerId); return resolve(); }
       }, 100);
       setTimeout(() => {
         clearTimeout(timerId);
