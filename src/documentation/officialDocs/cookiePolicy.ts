220ca03bf92988831f6f6d2e0fdbc34ef95a5343
Amine Harty <aminos.adm@gmail.com>
2021-12-01 13:01:38 +0100

    fix: delete useless code

--- src/documentation/officialDocs/cookiePolicy.ts
+++ src/documentation/officialDocs/cookiePolicy.ts
@@ -1,8 +1,6 @@
-import { singleton } from "aurelia-framework";
-
-@singleton(false)
-export abstract class cookiePolicy {
-  activate(): void {
-    window.location.href = "https://assets.website-files.com/608bd350d67fe62ab7818c74/619b9c4ae6d73869fac95783_Prime%20-%20Cookie%20Policy.pdf";
+export class CookiePolicy {
+  canActivate(): void {
+    window.open("https://assets.website-files.com/608bd350d67fe62ab7818c74/619b9c4ae6d73869fac95783_Prime%20-%20Cookie%20Policy.pdf", "_blank", "noopener noreferrer");
   }
 }
+
