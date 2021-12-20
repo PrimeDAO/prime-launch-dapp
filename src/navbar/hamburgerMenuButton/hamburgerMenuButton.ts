d22808fd6b83acf4ff1795717c0a4349ccfdb465
Gilad <gilad@tsabar.net>
2021-12-20 14:49:30 +0100

    added toggle function

--- src/navbar/hamburgerMenuButton/hamburgerMenuButton.ts
+++ src/navbar/hamburgerMenuButton/hamburgerMenuButton.ts
@@ -1,4 +1,19 @@
+import { bindable } from "aurelia-framework";
 import "./hamburgerMenuButton.scss";
 
 export class HamburgerMenuButton {
+  @bindable private isOpen = false;
+
+  private toggleMenu() {
+    this.isOpen = !this.isOpen;
+    console.log("open");
+  }
+  private openMenu() {
+    this.isOpen = true;
+    console.log("open");
+  }
+  private closeMenu() {
+    this.isOpen = false;
+    console.log("close");
+  }
 }
