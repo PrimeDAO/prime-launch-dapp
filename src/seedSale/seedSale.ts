b70cc49a3c0bb2d6edec55034730486d37e3fb2a
Eugene Allenov <e.allenov@gmail.com>
2022-06-06 19:32:56 +0700

    add connect() function

--- src/seedSale/seedSale.ts
+++ src/seedSale/seedSale.ts
@@ -1,7 +1,36 @@
 /* eslint-disable linebreak-style */
 import "./seedSale.scss";
+import { autoinject, containerless, customElement, singleton } from "aurelia-framework";
+import { Address, EthereumService } from "services/EthereumService";
+import { EventAggregator } from "aurelia-event-aggregator";
+import { DisposableCollection } from "services/DisposableCollection";
+import { TransactionReceipt } from "services/TransactionsService";
 
+enum Phase {
+    None = "None",
+    Mining = "Mining",
+    Confirming = "Confirming"
+}
+
+@autoinject
 export class SeedSale {
+    subscriptions: DisposableCollection = new DisposableCollection();
+    private accountAddress: Address = null;
+    private txPhase = Phase.None;
+    private txReceipt: TransactionReceipt;
 
+    constructor(
+      private ethereumService: EthereumService,
+      private eventAggregator: EventAggregator,
+    ){
+        this.subscriptions.push(this.eventAggregator.subscribe("Network.Changed.Account", async (account: Address) => {
+        this.accountAddress = account;
+        this.txPhase = Phase.None;
+        this.txReceipt = null;
+      }));
+    }
+    connect(): void {
+        this.ethereumService.ensureConnected();
+    }
 }
 
