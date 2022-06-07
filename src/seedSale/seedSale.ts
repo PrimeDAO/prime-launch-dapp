9b1ba4dbd13a71bec126d293795df011bf673d89
Eugene Allenov <e.allenov@gmail.com>
2022-06-07 21:55:55 +0700

    start bind seed info

--- src/seedSale/seedSale.ts
+++ src/seedSale/seedSale.ts
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
 import "./seedSale.scss";
-import { autoinject, containerless, customElement, singleton } from "aurelia-framework";
+import { autoinject } from "aurelia-framework";
 import { Router } from "aurelia-router";
 import { Seed } from "entities/Seed";
 import { Address, EthereumService } from "services/EthereumService";
@@ -14,11 +14,12 @@ import { Utils } from "services/utils";
 import { EventConfigException } from "services/GeneralEvents";
 import { GeoBlockService } from "services/GeoBlockService";
 import { BigNumber } from "ethers";
+import { DateService } from "services/DateService";
 
 enum Phase {
-    None = "None",
-    Mining = "Mining",
-    Confirming = "Confirming"
+  None = "None",
+  Mining = "Mining",
+  Confirming = "Confirming"
 }
 
 @autoinject
@@ -27,9 +28,9 @@ export class SeedSale {
   subscriptions: DisposableCollection = new DisposableCollection();
   loading: boolean;
   seed: Seed;
-  geoBlocked: boolean;
   userFundingTokenAllowance: BigNumber;
-  
+  percentsRaised: number
+
   private accountAddress: Address = null;
   private txPhase = Phase.None;
   private txReceipt: TransactionReceipt;
@@ -42,12 +43,35 @@ export class SeedSale {
     private router: Router,
     private congratulationsService: CongratulationsService,
     private geoBlockService: GeoBlockService,
+    private dateService: DateService,
   ){
     this.subscriptions.push(this.eventAggregator.subscribe("Network.Changed.Account", async (account: Address) => {
-    this.accountAddress = account;
-    this.txPhase = Phase.None;
-    this.txReceipt = null;
+      this.accountAddress = account;
+      this.txPhase = Phase.None;
+      this.txReceipt = null;
     }));
+  }
+
+  get timeLeft(): string {
+    let ms = this.seed.startsInMilliseconds;
+    const days = ms>86400000 ? Math.floor(ms / 86400000) : 0;
+    ms = ms>86400000 ? ms % 86400000 : ms;
+    const hrs = ms>3600000 ? Math.floor(ms / 3600000) : 0;
+    ms = ms>3600000 ? ms % 3600000 : ms;
+    const mins = ms>60000 ? Math.floor(ms / 60000) : 0;
+    const result = `${days}day${days > 1 ? "s" : ""}, ${hrs}h, ${mins}m`;
+    console.log("THIS TIME", result);
+    return result;
+  }
+
+  get seedTarget(): number {
+    return this.seed.target.toNumber() / 1000000;
+  }
+
+  async getPercentOfRaised():Promise<void> {
+    const percent = Math.floor(this.seed.target.toNumber() / 100);
+    const percentsRaised = Math.floor(this.seed.amountRaised.toNumber() / percent);
+    this.percentsRaised = percentsRaised;
   }
 
   connect(): void {
@@ -56,28 +80,21 @@ export class SeedSale {
 
   async activate(params: { address: Address}): Promise<void> {
     this.address = params.address;
-    this.geoBlocked = this.geoBlockService.blackisted;
   }
 
   async show1(): Promise<void> {
     await Utils.waitUntilTrue(() => true, 5000);
-    this.congratulationsService.show(`it's works!`);
+    this.congratulationsService.show("it's works!");
   }
 
   async show2(): Promise<void> {
     await Utils.waitUntilTrue(() => true, 5000);
-    this.congratulationsService.show(`it's works!`);
+    this.congratulationsService.show("it's works!");
   }
 
   async show3(): Promise<void> {
     await Utils.waitUntilTrue(() => true, 5000);
-    this.congratulationsService.show(`it's works!`);
-  }
-
-  async hydrateUserData(): Promise<void> {
-    if (this.ethereumService.defaultAccountAddress) {
-      this.userFundingTokenAllowance = await this.seed.fundingTokenAllowance();
-    }
+    this.congratulationsService.show("it's works!");
   }
 
   async attached(): Promise<void> {
@@ -104,9 +121,7 @@ export class SeedSale {
         await seed.ensureInitialized();
       }
       this.seed = seed;
-      this.geoBlocked = this.geoBlocked && this.seed.metadata.launchDetails.geoBlock;
-      await this.hydrateUserData();
-
+      this.getPercentOfRaised();
       //this.disclaimSeed();
 
     } catch (ex) {
@@ -117,7 +132,9 @@ export class SeedSale {
         this.eventAggregator.publish("launches.loading", false);
       }
       this.loading = false;
-      console.log('THIS SEED', this.seed)
+      console.log("THIS SEED", this.seed);
+      console.log("THIS TIME", this.timeLeft);
+      console.log("THIS v", this.percentsRaised);
     }
   }
 }
