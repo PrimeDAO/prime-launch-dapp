50b80651da01d48253d5b8d5dc71d0f1016a9863
Eugene Allenov <e.allenov@gmail.com>
2022-07-05 13:23:39 +0300

    fix netlify build

--- webpack.config.js
+++ webpack.config.js
@@ -282,9 +282,6 @@ module.exports = ( { production } = {}, { extractCss, analyze, tests, hmr, port,
     plugins: [
       ...when( !tests, new DuplicatePackageCheckerPlugin() ),
       new AureliaPlugin(),
-      new ModuleDependenciesPlugin( {
-        'aurelia-testing': [ './compile-spy', './view-spy' ]
-      } ),
       new HtmlWebpackPlugin( {
         template: 'index.ejs',
         filename: "../index.html",
