ecfd58254dd04c4e0305d21ea3e8489e2b4618a6
Eugene Allenov <e.allenov@gmail.com>
2022-08-10 13:37:22 +0700

    add wallet menu

--- src/resources/elements/ConnectButton/ConnectButton.ts
+++ src/resources/elements/ConnectButton/ConnectButton.ts
@@ -29,6 +29,7 @@ export class ConnectButton {
   private network: string;
   private networkSymbol: string;
   @bindable.booleanAttr private hideBalances: boolean;
+  showingWalletMenu = false;
 
   constructor(
     private ethereumService: EthereumService,
@@ -81,5 +82,9 @@ export class ConnectButton {
     if (this.txReceipt) {
       Utils.goto(this.ethereumService.getEtherscanLink(this.txReceipt.transactionHash, true));
     }
+  }
+
+  handleShowWalletMenu(): void {
+    this.showingWalletMenu = !this.showingWalletMenu;
   }
 }
