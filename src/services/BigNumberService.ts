1c04f34506389996c90d1f64528f1afe4c6cac1d
hdn <hieudn5293@yahoo.de>
2022-09-23 18:45:02 +0200

    feat(services): add min to BigNumber

--- src/services/BigNumberService.ts
+++ src/services/BigNumberService.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from "bignumber.js";
+import { BigNumber as EthersBigNumber } from "ethers";
 
 BigNumber.config({
   EXPONENTIAL_AT: [-100, 100],
@@ -8,3 +9,38 @@ BigNumber.config({
 
 export const toBigNumberJs = (n: unknown): BigNumber => new BigNumber(n?.toString());
 export default BigNumber;
+
+export class BigNumberService {
+  public min(input: EthersBigNumber[]): EthersBigNumber {
+
+    let minNumber = input[0];
+
+    if (input.length === 1) {
+      return input[0];
+    }
+
+    if (input.length === 2) {
+      minNumber = this.min2(input[0], input[1]);
+      return minNumber;
+    }
+
+
+    for (let i = 0; i < input.length - 1; i++) {
+      const first = input[i];
+      const second = input[i+1];
+      minNumber = this.min2(first, second);
+    }
+
+    return minNumber;
+  }
+
+  public min2(first: EthersBigNumber, second: EthersBigNumber): EthersBigNumber {
+    if (!first) return EthersBigNumber.from(0);
+    if (!second) return EthersBigNumber.from(0);
+
+    if (first.lt(second)) {
+      return first;
+    }
+    return second;
+  }
+}
