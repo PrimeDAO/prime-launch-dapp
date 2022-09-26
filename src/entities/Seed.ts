4e74a1c3b63850829df0615b5ce3d2ee4ff55c75
hdn <hieudn5293@yahoo.de>
2022-09-26 22:57:41 +0200

    fix(entities): switch order (was showing minus days)

--- src/entities/Seed.ts
+++ src/entities/Seed.ts
@@ -179,7 +179,7 @@ export class Seed implements ILaunch {
 
   @computedFrom("_now")
   public get endsInMilliseconds(): number {
-    return this.dateService.getDurationBetween(this._now, this.endTime).asMilliseconds();
+    return this.dateService.getDurationBetween(this.endTime, this._now).asMilliseconds();
   }
 
   @computedFrom("_now")
