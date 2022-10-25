4a0caf591a58e62ea9bff2b69f45e177b325c782
hdn <hieudn5293@yahoo.de>
2022-10-25 18:34:01 +0200

    test(services): init NumberService spec

---
+++ test/unit/services/NumberService.spec.ts
@@ -0,0 +1,41 @@
+import { BigNumber } from "ethers";
+import { NumberService } from "services/NumberService";
+
+describe("NumberService.spec", () => {
+  const numberService = new NumberService();
+
+  fdescribe("fromString", () => {
+    it("fromString", () => {
+      numberService.fromString(158.585559, 100000);/*?*/
+      numberService.fromString("158.585559", 100000);/*?*/
+      numberService.fromString(BigNumber.from(5), 100000);/*?*/
+    });
+
+    fit("toString", () => {
+      const input = 123.456789;
+
+      /** Guard cases */
+      expect(numberService.toString(NaN)).toBe(null);
+      expect(numberService.toString(input.toString())).toBe(input.toString());
+      expect(numberService.toString(input)).toBe("123.45");
+
+      /** Mantissa */
+      expect(numberService.toString(input, {mantissa: -1})).toBe(".4");
+      expect(numberService.toString(input, {mantissa: 0})).toBe("123");
+      expect(numberService.toString(input, {mantissa: 1})).toBe("123.4");
+      expect(numberService.toString(input, {mantissa: 10})).toBe("123.4567890000");
+
+      /** Average */
+      expect(numberService.toString(input, {average: true})).toBe("123.45");
+
+      /** ThousandSeparated */
+      const thousandInput = 1234.56789;
+      expect(numberService.toString(thousandInput, {thousandSeparated: true})).toBe("1,234.56");
+
+      /** Numeral JS */
+      const numeralJsBugInput = 0.0000001;
+      expect(numberService.toString(numeralJsBugInput, {thousandSeparated: true})).toBe("0.00");
+    });
+
+  });
+});
