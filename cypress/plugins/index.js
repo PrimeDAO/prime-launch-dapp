9a18b8ef59af5a48a6f7af7d7c1325e6de4eded2
hdn <hieudn5293@yahoo.de>
2022-11-03 16:45:50 +0100

    chore(cypress): init

---
+++ cypress/plugins/index.js
@@ -0,0 +1,70 @@
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/* eslint-disable */
+const { ProvidePlugin } = require("webpack");
+const webpack = require("@cypress/webpack-preprocessor");
+
+module.exports = (on, config) => {
+  // bind to the event we care about
+  // on('<event>', (arg1, arg2) => {
+  //   // plugin stuff here
+  // });
+  on("file:preprocessor", webpack({
+    webpackOptions: {
+      resolve: {
+        extensions: [".ts", ".js"],
+        alias: {
+          process: 'process/browser',
+        }
+      },
+      devtool: 'cheap-module-source-map',
+      devServer: {
+        client: {
+          overlay: false,
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: [/node_modules/],
+            use: [
+              {
+                loader: "ts-loader"
+              }
+            ]
+          },
+          {
+            test: /\.feature$/,
+            use: [
+              {
+                loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                options: config
+              }
+            ]
+          }
+        ]
+      },
+      plugins: [
+        new ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ]
+    }
+  }));
+
+  // require('@cypress/code-coverage/task')(on, config)
+  return config
+};
