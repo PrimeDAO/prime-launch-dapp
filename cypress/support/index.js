fd1ff70a9518d3ef8ed957bf79b2be7c42f79ab8
hdn <hieudn5293@yahoo.de>
2022-11-09 19:02:32 +0100

    e2e(support): hide url requests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -27,3 +27,19 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false
 })
+
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML = `
+    .command-name-request,
+    .command-name-new-url,
+    .command-name-xhr {
+      display: none
+    };
+  `
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
