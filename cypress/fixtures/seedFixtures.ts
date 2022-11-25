bc8d925d148a320b08154c6194fc87f329f63d35
hdn <hieudn5293@yahoo.de>
2022-11-25 12:07:20 +0100

    e2e(fixtures): update

--- cypress/fixtures/seedFixtures.ts
+++ cypress/fixtures/seedFixtures.ts
@@ -2,6 +2,7 @@ import {
   SeedBuilder,
   SeedClassesBuilder,
 } from "../integration/common/builders/E2eSeedBuilder";
+import { getRandomId } from "../integration/common/utilities";
 import {
   ALLOWLIST_CLASS_1,
   ALLOWLIST_CLASS_2,
@@ -31,9 +32,12 @@ export const MINIMUM_PERMISSIONED_SEED_WITH_CLASS = SeedBuilder.create(
 
 export const MINIMUM_SEED_CLASS = SeedClassesBuilder.create().seedClass;
 
-export const CLASS_1_WITH_ALLOWLISTED_ADDRESSES =
-  SeedClassesBuilder.create().withAllowlists(ALLOWLIST_CLASS_1).seedClass;
-export const CLASS_2_WITH_ALLOWLISTED_ADDRESSES =
-  SeedClassesBuilder.create().withAllowlists(ALLOWLIST_CLASS_2).seedClass;
-export const CLASS_3_WITH_ALLOWLISTED_ADDRESSES =
-  SeedClassesBuilder.create().withAllowlists(ALLOWLIST_CLASS_3).seedClass;
+export const CLASS_1_WITH_ALLOWLISTED_ADDRESSES = SeedClassesBuilder.create()
+  .withClassName(`Class_1_${getRandomId()}`)
+  .withAllowlists(ALLOWLIST_CLASS_1).seedClass;
+export const CLASS_2_WITH_ALLOWLISTED_ADDRESSES = SeedClassesBuilder.create()
+  .withClassName(`Class_2_${getRandomId()}`)
+  .withAllowlists(ALLOWLIST_CLASS_2).seedClass;
+export const CLASS_3_WITH_ALLOWLISTED_ADDRESSES = SeedClassesBuilder.create()
+  .withClassName(`Class_3_${getRandomId()}`)
+  .withAllowlists(ALLOWLIST_CLASS_3).seedClass;
